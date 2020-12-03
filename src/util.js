99502a962be1841c31359fa0fb1c33ff12af5ad8
Palak <palakkotwani2883@gmail.com>
2020-12-03 21:13:07 +0530

    Third Commit

--- src/util.js
+++ src/util.js
@@ -13,13 +13,13 @@ const casesTypeColors = {
     hex: "#7dd71d",
     rgb: "rgb(125, 215, 29)",
     half_op: "rgba(125, 215, 29, 0.5)",
-    multiplier: 1200,
+    multiplier: 1100,
   },
   deaths: {
     hex: "#fb4443",
     rgb: "rgb(251, 68, 67)",
-    half_op: "rgba(251, 68, 67, 0.5)",
-    multiplier: 2000,
+    half_op: "rgba(0, 68, 67, 0.4)",
+    multiplier: 1800,
   },
 };
 
